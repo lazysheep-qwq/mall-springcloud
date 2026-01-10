3bf6d8f60c5fbb44ea811c32d68733b612715753
lazysheep <2145017490@qq.com>
2026-01-10 17:23:13 +0800

    2-07

--- hm-common/src/main/java/com/hmall/common/interceptors/UserInfoInterceptor.java
+++ hm-common/src/main/java/com/hmall/common/interceptors/UserInfoInterceptor.java
@@ -2,25 +2,27 @@ package com.hmall.common.interceptors;
 
 import cn.hutool.core.util.StrUtil;
 import com.hmall.common.utils.UserContext;
-import org.checkerframework.checker.nullness.qual.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class UserInfoInterceptor implements HandlerInterceptor {
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String userInfo = request.getHeader("user-info");
 
-        String useInfo = request.getHeader("user-info");
-        if (StrUtil.isNotBlank(useInfo)){
-            UserContext.setUser(Long.valueOf(useInfo));
+        if (StrUtil.isNotBlank(userInfo)){
+            UserContext.setUser(Long.valueOf(userInfo));
         }
+
         return true;
     }
 
+
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         UserContext.removeUser();
     }
 }
